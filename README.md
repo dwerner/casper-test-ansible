c5c862ef81a1cf40e1e33426ec566fbb948f5ae6
Daniel Werner <dan@casperlabs.io>
2021-01-27 16:32:14 -0800

    expose tasks
    update casper-tool.py for versioned binaries
    
    copy bin in to versioned directory as well
    
    running network
    
    add stop-service
    
    implement bootstrap+validators+zero_weight_ops

--- README.md
+++ README.md
@@ -1,28 +1,23 @@
+AWS Cluster Ansible scripts
 
-```bash
+Dependencies:
 
-./casper-tool.py -b ../casper-node create-network hetzner-test/ 
+- aws-cli
+- ansible
 
-ansible -u root -i hosts.yaml known_nodes -m ping
+TODO:
+- create template for security group and instances
+- document helper scripts
 
-ansible-playbook -u root -i hosts.yaml -e '{"chain_path":"hetzner-test"}' upload_chainspec.yaml
-
-
-
-# TODO:
-# playbook which installs openvpn, grabs the client.ovpn, tc.key, and sets up firewall rules
-~# iptables -t nat -A POSTROUTING -o eth0 -s 10.8.0.0/24 -j MASQUERADE
-~# iptables -t nat -A POSTROUTING -o eth0 -s 10.^C.0/24 -j MASQUERADE
-~# iptables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT
-~# iptables -A INPUT -i tun0 -j ACCEPT
-~# iptables -A FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-~# ip6tables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT
-~# ip6tables -A INPUT -i tun0 -j ACCEPT
-~# ip6tables -A FORWARD -i tun0 -o eth0 -s 2a01:4f8:c2c:5fc7:80::/112 -m state --state NEW -j ACCEPT
-~# iptables-save > /etc/iptables.rules
-~# ip6tables-save > /etc/ip6tables.rules
 
 ```
+./build-binaries
+./build-chain
+./deploy "bootstrap, validators"
 
+./start-service bootstrap
+./start-service validators 
 
-./casper-tool.py -b ../casper-node create-network
+./stop-service bootstrap
+./clean bootstrap
+```
--- casper-tool.py
+++ casper-tool.py
@@ -8,20 +8,8 @@ import click
 import shutil
 import toml
 import yaml
-
-#: List of WASM blobs required to be set up in chainspec.
-CONTRACTS = [
-        "add_bid",
-        "withdraw_bid",
-        "delegate",
-        "undelegate",
-        "transfer_to_account_u512_stored",
-        "transfer_to_account_u512"
-]
-
-
-#: Relative directory to be appended to basedir in case WASM dir is not specified.
-DEFAULT_WASM_SUBDIR = ["target", "wasm32-unknown-unknown", "release"]
+from pathlib import Path
+from itertools import chain
 
 
 #: The port the node is reachable on.
@@ -34,12 +22,20 @@ NODE_PORT = 34553
     "--basedir",
     help="casper-node source code base directory",
     type=click.Path(exists=True, dir_okay=True, file_okay=False, readable=True),
-    default=os.path.join(os.path.dirname(__file__), "..", ".."),
+    default=os.path.join(os.path.dirname(__file__), "..", "casper-node"),
+)
+@click.option(
+    "-l",
+    "--launcher",
+    help="casper-node-launcher source code base directory",
+    type=click.Path(exists=True, dir_okay=True, file_okay=False, readable=True),
+    default=os.path.join(os.path.dirname(__file__), "..", "casper-node-launcher"),
 )
 @click.option(
     "--casper-client",
     help="path to casper client binary (compiled from basedir by default)",
     type=click.Path(exists=True, dir_okay=False, readable=True),
+    default="../casper-node/target/release/casper-client",
 )
 @click.option(
     "-p",
@@ -59,22 +55,14 @@ NODE_PORT = 34553
     type=click.Path(exists=True, dir_okay=False, readable=True),
     help="Chainspec template to use",
 )
-@click.option(
-    "-w",
-    "--wasm-dir",
-    type=click.Path(exists=True, dir_okay=False, readable=True),
-    help="directory containing compiled wasm contracts (defaults to `BASEDIR/{}`".format(
-        os.path.join(*DEFAULT_WASM_SUBDIR)
-    ),
-)
 @click.pass_context
 def cli(
     ctx,
     basedir,
+    launcher,
     production,
     chainspec_template,
     config_template,
-    wasm_dir,
     casper_client,
 ):
     """Casper Network creation tool
@@ -91,7 +79,6 @@ def cli(
         obj["chainspec_template"] = os.path.join(
             basedir, "resources", "local", "chainspec.toml.in"
         )
-    obj["wasm_dir"] = wasm_dir or os.path.join(basedir, *DEFAULT_WASM_SUBDIR)
 
     if config_template:
         obj["config_template"] = chainspec_template
@@ -115,18 +102,23 @@ def cli(
             "--",
         ]
 
+    obj["casper-node-bin"] = \
+            os.path.join(basedir, "target", "release", "casper-node")
+    obj["casper-node-launcher-bin"] = \
+            os.path.join(launcher, "target", "release", "casper-node-launcher")
+
     ctx.obj = obj
     return
 
 
 @cli.command("create-network")
 @click.pass_obj
-@click.argument("target-path", type=click.Path(exists=False, writable=True), default="chain-1")
+@click.argument("target-path", type=click.Path(exists=False, writable=True), default="artifacts/chain-1")
 @click.option(
     "-k",
     "--hosts-file",
-    help="Parse an hosts.yaml file, using all.children.known_nodes for set of known nodes",
-    default="hosts.yaml"
+    help="Parse an hosts.yaml file, using all.children.validators for set of known nodes",
+    default="aws-hosts.yaml"
 )
 @click.option(
     "-n",
@@ -140,78 +132,170 @@ def cli(
     default=300,
     type=int,
 )
+@click.option(
+    "-v",
+    "--node-version",
+    type=str,
+    help="semver with underscores e.g. 1_0_0",
+    default="1_0_0"
+)
 def create_network(
     obj,
     target_path,
     hosts_file,
     network_name,
     genesis_in,
+    node_version,
 ):
     if not network_name:
         network_name = os.path.basename(os.path.join(target_path))
 
     # Create the network output directories.
     show_val("Output path", target_path)
-    os.mkdir(target_path)
-    chain_path = os.path.join(target_path, "chain")
-    os.mkdir(chain_path)
 
-    # Prepare paths and copy over all contracts.
-    show_val("WASM contracts", obj["wasm_dir"])
-    contract_paths = {}
-    for contract in CONTRACTS:
-        source = os.path.join(obj["wasm_dir"], "{}.wasm".format(contract))
-        target = os.path.join(chain_path, "{}.wasm".format(contract))
-        shutil.copy(source, target)
-        basename = "{}.wasm".format(contract)
+    nodes_path = \
+            os.path.join(target_path, "nodes")
 
-        # We use relative paths when creating a self-contained network.
-        contract_paths[contract] = basename
+    staging_path = os.path.join(target_path, "staging")
+    bin_path = \
+            os.path.join(staging_path, "bin")
+    bin_version_path = \
+            os.path.join(staging_path, "bin", node_version)
+    config_path = \
+            os.path.join(staging_path, "config")
+
+    # Staging directories for config, chain
+    show_val("Node version", node_version)
+
+    Path(nodes_path).mkdir(parents=True)
+    Path(bin_path).mkdir(parents=True)
+    Path(bin_version_path).mkdir(parents=True)
+    Path(config_path).mkdir(parents=True)
 
     # Update chainspec values.
     chainspec = create_chainspec(
-        obj["chainspec_template"], network_name, genesis_in, contract_paths
+        obj["chainspec_template"], network_name, genesis_in
     )
 
-    chainspec_path = os.path.join(chain_path, "chainspec.toml")
+    # Dump chainspec into staging dir
+    chainspec_path = os.path.join(config_path, "chainspec.toml")
     toml.dump(chainspec, open(chainspec_path, "w"))
     show_val("Chainspec", chainspec_path)
 
+    # Copy casper-node into bin/VERSION/ staging dir
+    node_bin_path = os.path.join(bin_version_path, "casper-node")
+    shutil.copyfile(obj["casper-node-bin"], node_bin_path)
+    os.chmod(node_bin_path, 0o744)
+
+    # Copy casper-node into bin/ staging dir
+    launcher_bin_path = os.path.join(bin_path, "casper-node-launcher")
+    shutil.copyfile(obj["casper-node-launcher-bin"], launcher_bin_path)
+    os.chmod(launcher_bin_path, 0o744)
+
+    # Load validators from ansible yaml inventory
+    hosts = yaml.load(open(hosts_file), Loader=yaml.FullLoader)
+
     # Setup each node, collecting all pubkey hashes.
     show_val("Node config template", obj["config_template"])
-    pubkeys = list()
 
-    hosts = yaml.load(open(hosts_file), Loader=yaml.FullLoader)
-    known_nodes = hosts["all"]["children"]["known_nodes"]["hosts"]
+    validator_nodes = list(hosts["all"]["children"]["validators"]["hosts"].keys())
+    bootstrap_nodes = list(hosts["all"]["children"]["bootstrap"]["hosts"].keys())
+    zero_weight_nodes = list(hosts["all"]["children"]["zero_weight"]["hosts"].keys())
 
-    os.mkdir(os.path.join(target_path, "nodes"))
+    bootstrap_keys = list()
+    validator_keys = list()
+    zero_weight_keys = list()
 
-    for public_address in known_nodes:
-        show_val("known node", public_address)
+    for public_address in bootstrap_nodes:
+        key_path = os.path.join(nodes_path, public_address, "etc", "casper", "keys")
+        account = generate_account_key(key_path, public_address, obj)
+        generate_node(bootstrap_nodes, obj, nodes_path, node_version, public_address)
+        validator_keys.append(account)
 
-        node_path = os.path.join(target_path, "nodes/{}".format(public_address))
-        os.mkdir(node_path)
-        pubkey_hex = create_node(
-            public_address,
-            obj["casper_client_argv0"],
-            network_name,
-            obj["config_template"],
-            node_path,
-            list(node for node in known_nodes if node != public_address),
+    for public_address in validator_nodes:
+        key_path = os.path.join(nodes_path, public_address, "etc", "casper", "keys")
+        account = generate_account_key(key_path, public_address, obj)
+        generate_node(
+            ["{}:{}".format(n, NODE_PORT) for n in validator_nodes if n != public_address],
+            obj, nodes_path, node_version, public_address)
+        validator_keys.append(account)
+
+    for public_address in zero_weight_nodes:
+        key_path = os.path.join(nodes_path, public_address, "etc", "casper", "keys")
+        account = generate_account_key(key_path, public_address, obj)
+        generate_node(
+            ["{}:{}".format(n, NODE_PORT) for n in bootstrap_nodes + validator_nodes],
+            obj, nodes_path, node_version, public_address)
+        zero_weight_keys.append(account)
+
+    faucet_path = os.path.join(staging_path, "faucet")
+    faucet_key = generate_account_key(faucet_path, "faucet", obj)
+
+    accounts_path = os.path.join(config_path, "accounts.csv")
+    # Copy accounts.csv into staging dir
+    create_accounts_csv(open(accounts_path, "w"), faucet_key, bootstrap_keys + validator_keys, zero_weight_keys)
+
+    for public_address in bootstrap_nodes + validator_nodes + zero_weight_nodes:
+        node_path = os.path.join(nodes_path, public_address)
+
+        # copy the bin and chain into each node's versioned fileset
+        node_var_lib_casper = os.path.join(node_path, "var", "lib", "casper")
+        node_bin_path = \
+                os.path.join(node_var_lib_casper, "bin")
+        Path(node_var_lib_casper).mkdir(parents=True)
+        shutil.copytree(bin_path, node_bin_path)
+
+        # should already exist
+        node_config_path = \
+                os.path.join(node_path, "etc", "casper", node_version)
+
+        node_key_path = os.path.join(node_path, "etc", "casper", "keys")
+
+        # copy the faucet's secret_key.pem into each node's config
+        faucet_target_path = os.path.join(node_key_path, "faucet")
+        Path(faucet_target_path).mkdir(parents=True)
+        shutil.copyfile(
+            os.path.join(faucet_path, "secret_key.pem"),
+            os.path.join(faucet_target_path, "secret_key.pem")
         )
-        pubkeys.append(pubkey_hex)
 
-    accounts_path = os.path.join(chain_path, "accounts.csv")
-    show_val("accounts file", accounts_path)
-    create_accounts_csv(open(accounts_path, "w"), pubkeys)
+        for filename in os.listdir(config_path):
+            shutil.copyfile(
+                os.path.join(config_path, filename),
+                os.path.join(node_config_path, filename)
+            )
 
 
-def create_chainspec(template, network_name, genesis_in, contract_paths):
+def generate_account_key(key_path, public_address, obj):
+    run_client(obj["casper_client_argv0"], "keygen", key_path)
+    pubkey_hex = open(os.path.join(key_path, "public_key_hex")).read().strip()
+    return pubkey_hex
+
+
+def generate_node(known_addresses, obj, nodes_path, node_version, public_address):
+    node_path = os.path.join(nodes_path, public_address)
+    node_config_path = \
+        os.path.join(node_path, "etc", "casper", node_version)
+    Path(node_config_path).mkdir(parents=True)
+    config = toml.load(open(obj["config_template"]))
+    config["node"]["chainspec_config_path"] = "chainspec.toml"
+    config["consensus"]["secret_key_path"] = os.path.join("..", "keys", "secret_key.pem")
+    # add faucet to the `faucet` subfolder in keys
+    config["logging"]["format"] = "json"
+    config["network"]["public_address"] = "{}:{}".format(public_address, NODE_PORT)
+    config["network"]["bind_address"] = "0.0.0.0:{}".format(NODE_PORT)
+    config["network"]["known_addresses"] = ["{}:{}".format(n, NODE_PORT) for n in known_addresses]
+    # Setup for volume operation.
+    storage_path = "/storage/{}".format(public_address)
+    config["storage"]["path"] = storage_path
+    config["consensus"]["unit_hashes_folder"] = storage_path
+    toml.dump(config, open(os.path.join(node_config_path, "config.toml", ), "w"))
+
+
+def create_chainspec(template, network_name, genesis_in):
     """Creates a new chainspec from a template.
-
     `contract_path` must be a dictionary mapping the keys of `CONTRACTS` to relative or absolute
     paths to be put into the new chainspec.
-
     Returns a dictionary that can be serialized using `toml`.
     """
     show_val("Chainspec template", template)
@@ -221,71 +305,64 @@ def create_chainspec(template, network_name, genesis_in, contract_paths):
     genesis_timestamp = (datetime.utcnow() + timedelta(seconds=genesis_in)).isoformat(
         "T"
     ) + "Z"
-
-    # Update the chainspec.
     show_val("Genesis", "{} (in {} seconds)".format(genesis_timestamp, genesis_in))
     chainspec["genesis"]["name"] = network_name
     chainspec["genesis"]["timestamp"] = genesis_timestamp
-
-    # Setup WASM contracts.
-    for contract in CONTRACTS:
-        key = "{}_installer_path".format(contract)
-        chainspec["genesis"][key] = contract_paths[contract]
-
-    # Use a relative path for accounts.csv
     chainspec["genesis"]["accounts_path"] = "accounts.csv"
-
     return chainspec
 
 
 def create_node(
-    public_address, client_argv0, network_name, config_template, node_path, known_nodes
+    public_address, client_argv0, config_template, node_path, validators
 ):
     """Create a node configuration inside a network.
-
     Paths are assumed to be set up using `create_chainspec`.
-
     Returns the nodes public key as a string."""
-
     # Generate a key
     key_path = os.path.join(node_path, "keys")
     run_client(client_argv0, "keygen", key_path)
 
     config = toml.load(open(config_template))
-    config["node"]["chainspec_config_path"] = "../chain/chainspec.toml"
-
+    config["node"]["chainspec_config_path"] = "chainspec.toml"
     config["consensus"]["secret_key_path"] = os.path.join(
         os.path.relpath(key_path, node_path), "secret_key.pem"
     )
-
     config["logging"]["format"] = "json"
-
     # Set the public address to `casper-node-XX`, which will resolve to the internal
     # network IP, and use the automatic port detection by setting `:0`.
     config["network"]["public_address"] = "{}:{}".format(public_address, NODE_PORT)
     config["network"]["bind_address"] = "0.0.0.0:{}".format(NODE_PORT)
-
     config["network"]["known_addresses"] = [
         "{}:{}".format(n, NODE_PORT)
-        for n in known_nodes
+        for n in validators
     ]
-
     # Setup for volume operation.
-    config["storage"]["path"] = "/storage"
-    config["consensus"]["unit_hashes_folder"] = "/storage"
-
-    toml.dump(config, open(os.path.join(node_path, "config.toml"), "w"))
-
+    storage_path = "/storage/{}".format(public_address)
+    config["storage"]["path"] = storage_path
+    config["consensus"]["unit_hashes_folder"] = storage_path
+    toml.dump(config, open(os.path.join(node_path, "config.toml", ), "w"))
     return open(os.path.join(key_path, "public_key_hex")).read().strip()
 
 
-def create_accounts_csv(output_file, pubkeys):
-    items = pubkeys
-    items.sort()
-    for key_hex in items:
-        motes = 1000000000000000
-        weight = 10000000000000
-        output_file.write("{},{},{}\n".format(key_hex, motes, weight))
+def create_accounts_csv(output_file, faucet, validators, zero_weight_ops):
+    """
+    :param output_file: accounts.csv
+    :param faucet: public key of faucet account
+    :param validators: public keys of validators with weight
+    :param zero_weight_ops: public keys of zero weight operators
+    :return: output_file will be an appropriately formatted csv
+    """
+    output_file.write("{},{},{}\n".format(faucet, 10**32, 0))
+
+    for index, key_hex in enumerate(validators):
+        motes = 10**32
+        staking_weight = 10**13 + index
+        output_file.write("{},{},{}\n".format(key_hex, motes, staking_weight))
+
+    for key_hex in zero_weight_ops:
+        motes = 10**32
+        staking_weight = 0
+        output_file.write("{},{},{}\n".format(key_hex, motes, staking_weight))
 
 
 def run_client(argv0, *args):
