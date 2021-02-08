1b282f2db0a16ae0047b7058d798d85d4389d2df
Daniel Werner <dan@casperlabs.io>
2021-02-07 21:15:44 -0800

    tag:name aws instances danw-test-[PublicIpAddress]

--- tag_instances.py
+++ tag_instances.py
@@ -3,16 +3,19 @@ import boto3
 ec2 = boto3.client('ec2')
 response = ec2.describe_instances()
 instance_names = {}
+instance_ips = {}
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
         instance_id = instance["InstanceId"]
+        instance_ip_addr = instance["PublicIpAddress"]
         for tag in instance["Tags"]:
             if tag["Key"] == "Name" and tag["Value"].startswith("danw-test"):
                 instance_names[instance_id] = tag["Value"]
+                instance_ips[instance_id] = instance_ip_addr
 
 operations = []
-for num, (instance_id, old_name) in enumerate(instance_names.items()):
-    name = "danw-test-{0:03d}".format(num)
+for instance_id, old_name in instance_ips.items():
+    name = "danw-test-{}".format(instance_ips[instance_id])
     if old_name != name:
         operations.append((instance_id, {"Key":"Name", "Value": name }))
 
@@ -24,6 +27,11 @@ else:
 answer = input("Would you like to proceed with these changes? [y/N]")
 if answer == "y":
     for instance_id, tag in operations:
-        ec2.create_tags(Resources=[instance_id], Tags=[tag])
-
-print("All done.")
+        ec2.create_tags(
+            DryRun=True,
+            Resources=[instance_id],
+            Tags=[tag]
+        )
+    print("All done.")
+else:
+    print("Nothing to do")
